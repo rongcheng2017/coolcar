19a00c0cfef99d20608447ed27f423f0ea686847
丰荣成 <fengrongcheng@qihoodeMacBook-Pro.local>
2021-09-09 16:54:56 +0800

    配置profile服务

--- server/rental/main.go
+++ server/rental/main.go
@@ -3,11 +3,13 @@ package main
 import (
 	"context"
 	rentalpb "coolcar/rental/api/gen/v1"
+	"coolcar/rental/profile"
+	profiledao "coolcar/rental/profile/dao"
 	"coolcar/rental/trip"
 	"coolcar/rental/trip/client/car"
 	"coolcar/rental/trip/client/poi"
-	"coolcar/rental/trip/client/profile"
-	"coolcar/rental/trip/dao"
+	profileClient "coolcar/rental/trip/client/profile"
+	tripdao "coolcar/rental/trip/dao"
 	"coolcar/shared/server"
 	"log"
 
@@ -26,10 +28,10 @@ func main() {
 	mongoClient, err := mongo.Connect(c, options.Client().ApplyURI("mongodb://localhost:27017/coolcar?readPreference=primary&appname=mongodb-vscode%200.6.10&directConnection=true&ssl=false"))
 	if err != nil {
 		logger.Fatal("connot connect mongdb", zap.Error(err))
-	}else {
+	} else {
 		logger.Info("connect mongo db success")
 	}
-
+	db := mongoClient.Database("coolcar")
 	err = server.RunGRPCServer(&server.GRPCConfig{
 		Name:              "rental",
 		Logger:            logger,
@@ -38,13 +40,17 @@ func main() {
 		RegisterFunc: func(s *grpc.Server) {
 			rentalpb.RegisterTripServiceServer(s, &trip.Service{
 				CarManager:     &car.Manager{},
-				ProfileManager: &profile.Manager{},
+				ProfileManager: &profileClient.Manager{},
 				POIManager:     &poi.Manager{},
-				Mongo:          dao.NewMongo(mongoClient.Database("coolcar")),
+				Mongo:          tripdao.NewMongo(db),
 				Logger:         logger,
 			})
-
+			rentalpb.RegisterProfileServiceServer(s, &profile.Service{
+				Mongo:  profiledao.NewMongo(db),
+				Logger: logger,
+			})
 		},
 	})
-	logger.Fatal("cannot start server", zap.Error(err))
+	logger.Fatal("cannot start trip server", zap.Error(err))
+
 }
---
+++ wx/miniprogram/service/profile.ts
@@ -0,0 +1,33 @@
+import { rental } from "./proto_gen/rental/rental_pb";
+import { Coolcar } from "./request";
+
+export namespace ProfileService {
+    export function getProfile(): Promise<rental.v1.IProfile> {
+
+        return Coolcar.sendRequesWithAutyRetry(
+            {
+                method: 'GET',
+                path: '/v1/profile',
+                respMarshaller: rental.v1.Profile.fromObject,
+            }
+        )
+    }
+
+    export function submitProfile(req: rental.v1.Identity): Promise<rental.v1.IProfile> {
+
+        return Coolcar.sendRequesWithAutyRetry({
+            method: "POST",
+            path: '/v1/profile',
+            data: req,
+            respMarshaller: rental.v1.Profile.fromObject
+        })
+    }
+
+    export function clearProflie(): Promise<rental.v1.IProfile> {
+        return Coolcar.sendRequesWithAutyRetry({
+            method: 'DELETE',
+            path: '/v1/profile',
+            respMarshaller: rental.v1.Profile.fromObject,
+        })
+    }
+}
